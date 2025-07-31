345d7eb656b7c6d90e7f0ea2479cfb4cb26da1d9
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-07-30 23:41:38 -0300

    new method inplementated

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,13 +16,26 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("## First Test: Seller Find By Id ##");
-		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+
+		System.out.println("## Second Test: Seller Find By Department");
+		Department departmentTest = new Department(2, null);
+		List<Seller> listSeller = sellerDao.findByDepartment(departmentTest);
+		for (Seller s : listSeller) {
+			System.out.println(s);
+		}
 		
+		System.out.println("## Third Test: Seller Find All");
+		List<Seller> listSellerFindAll = sellerDao.findAll();
+		for (Seller s : listSellerFindAll) {
+			System.out.println(s);
+		}
+		
+		System.out.println("## Fourth Test: Seller insert");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 5400.00, departmentTest);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new Id = " + newSeller.getId());
 	}
 
 }
-
-
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,10 +24,44 @@ public class SellerDaoJdbc implements SellerDao {
 		this.conn = conn;
 	}
 	
-	
 	@Override
-	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+	public void insert(Seller seller) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"INSERT INTO seller "
+				+"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+				+"VALUES "
+				+"(? ,? ,? ,?, ?)",
+				Statement.RETURN_GENERATED_KEYS
+				);
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+			st.setDouble(4, seller.getBaseSalary());
+			st.setInt(5, seller.getDepartment().getId());
+		
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					seller.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else
+			{
+				throw new DbException("Unexpected erro! No rows affected!");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -95,8 +130,44 @@ public class SellerDaoJdbc implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"ORDER BY Name"
+					);
+
+			rs = st.executeQuery();
+			
+			List<Seller> resultsList = new ArrayList<>();
+			Map<Integer, Department> mapDepartment = new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Department dep = mapDepartment.get(rs.getInt("DepartmentId"));
+				
+				if(dep==null) {
+					dep = instantiateDepartment(rs);
+					mapDepartment.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller seller = instantiateSeller(rs, dep);
+				resultsList.add(seller);
+				
+			}
+			return resultsList;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 
@@ -112,7 +183,6 @@ public class SellerDaoJdbc implements SellerDao {
 					+"ON seller.DepartmentId = department.Id "
 					+"where DepartmentId = ? "
 					+"ORDER BY Name"
-				
 					);
 			
 			st.setInt(1, department.getId());
