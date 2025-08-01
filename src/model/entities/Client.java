076feea7d7c732bd98373fb0bb5df80394bfacd9
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-07-31 23:53:43 -0300

    new class client entity

---
+++ src/model/entities/Client.java
@@ -0,0 +1,83 @@
+package model.entities;
+
+final class Client {
+	
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private String telNumber;
+	
+	public Client() {
+	}
+
+	public Client(Integer id, String name, String email, String telNumber) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.telNumber = telNumber;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelNumber() {
+		return telNumber;
+	}
+
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", email=" + email + ", telNumber=" + telNumber + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
