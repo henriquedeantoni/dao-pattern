45630b181db22abc1b097f6d8c218ffc7ed38293
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-13 07:47:21 -0300

    feat: created order entity model

--- src/model/entities/Order.java
+++ src/model/entities/Order.java
@@ -9,4 +9,89 @@ public class Order {
 	private double totalPrice;
 	private OrderStatus orderStatus;
 	
+	private Seller seller;
+	private Client client;
+	
+	public Order() {
+	}
+
+	public Order(int id, int totalHours, double totalPrice, OrderStatus orderStatus, Seller seller, Client client) {
+		super();
+		this.id = id;
+		this.totalHours = totalHours;
+		this.totalPrice = totalPrice;
+		this.orderStatus = orderStatus;
+		this.seller = seller;
+		this.client = client;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTotalHours() {
+		return totalHours;
+	}
+
+	public void setTotalHours(int totalHours) {
+		this.totalHours = totalHours;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public Seller getSeller() {
+		return seller;
+	}
+
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
