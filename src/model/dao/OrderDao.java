06b7c2ce7e8ffc66638d7cfc05173b5f30697085
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-13 07:46:25 -0300

    feat: created order dao interface

--- src/model/dao/OrderDao.java
+++ src/model/dao/OrderDao.java
@@ -1,5 +1,17 @@
 package model.dao;
 
-public class OrderDao {
+import java.util.List;
 
+import model.entities.Client;
+import model.entities.Order;
+import model.entities.enums.OrderStatus;
+
+public interface OrderDao {
+	void insert(Order obj);
+	void update(Order obj);
+	void delete(Order obj);
+	Order findById(Integer id);
+	List<Order> findByMinimumValue(Double minValue);
+	List<Order> findByOrderStatus(OrderStatus orderStatus);
+	List<Order> findByClient(Client client);
 }
