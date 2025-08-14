2e9d28902991ad771c40677e1b0750113a8dc6a5
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-13 21:52:38 -0300

    feat: update order dao interface

--- src/model/dao/OrderDao.java
+++ src/model/dao/OrderDao.java
@@ -1,9 +1,11 @@
 package model.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import model.entities.Client;
 import model.entities.Order;
+import model.entities.Seller;
 import model.entities.enums.OrderStatus;
 
 public interface OrderDao {
@@ -14,4 +16,8 @@ public interface OrderDao {
 	List<Order> findByMinimumValue(Double minValue);
 	List<Order> findByOrderStatus(OrderStatus orderStatus);
 	List<Order> findByClient(Client client);
+	List<Order> findBySeller(Seller seller);
+	List<Order> filterBySellerAndClient(Client client, Seller seller);
+	List<Order> filterByMonth(Date date);
+	List<Order> filterByYear(Date date);
 }
