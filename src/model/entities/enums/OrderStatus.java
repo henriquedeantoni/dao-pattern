bd77ebf5bd8df9fea11e4ed5edb9ca5dd8c62e0b
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-09-19 23:52:00 -0300

    feat: orderstatus modified

--- src/model/entities/enums/OrderStatus.java
+++ src/model/entities/enums/OrderStatus.java
@@ -1,6 +1,29 @@
 package model.entities.enums;
 
 public enum OrderStatus {
-	
+    ONAPPROVAL(1),
+    PAID(2),
+    DELIVERED(3),
+    CANCELED(4),
+    LOST(5);
+
+    private int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code: " + code);
+    }
 	
 }
