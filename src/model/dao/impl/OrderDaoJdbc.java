7f8a297a41519d3ab2c874b07d12fe9a172b47b3
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-09-20 01:48:29 -0300

    feat: new method on order dao jdbc

--- src/model/dao/impl/OrderDaoJdbc.java
+++ src/model/dao/impl/OrderDaoJdbc.java
@@ -1,8 +1,15 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.OrderDao;
 import model.entities.Client;
 import model.entities.Order;
@@ -11,10 +18,55 @@ import model.entities.enums.OrderStatus;
 
 public class OrderDaoJdbc implements OrderDao {
 
+	private Connection conn;
+	
+	public OrderDaoJdbc(Connection conn) {
+		this.conn = conn;
+	}	
+	
 	@Override
 	public void insert(Order obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			
+			st = conn.prepareStatement(
+					"INSERT INTO Department "
+					+"(TotalHoursService, Title, Description, TotalPrice, OrderStatus, PaymentMethod, SellerId, ClientId) "
+					+"VALUES "
+					+"(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS
+					);
+			st.setInt(1, obj.getTotalHoursService());
+			st.setString(2, obj.getTitle());
+			st.setString(3, obj.getDescription());
+			st.setDouble(4, obj.getTotalPrice());
+			st.setInt(5, obj.getOrderStatus().getCode());
+			st.setInt(6, obj.getPaymentMethod().getCode());
+			st.setInt(7, obj.getSeller().getId());
+			st.setInt(8, obj.getClient().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected>0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else
+			{
+				throw new DbException("Unexpected erro! No rows affected!");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeConnection();
+		}
 	}
 
 	@Override
